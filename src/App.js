5846055187ab2372ef452eda08f551a5b33b4207
ayush <rajayush746@gmail.com>
2024-06-15 17:47:16 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Editor from 'react-simple-code-editor';
+
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+
+import './prism-vcc-dark-plus.css';
 import './App.css';
 
+const codeSnippet = `function add(a, b) {
+  return a + b;
+}
+
+add(5, 10);
+// Output: 15
+// Type your code below
+`;
+
 function App() {
+  const [code, setCode] = useState(codeSnippet);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className='head'>
+        React code editor and syntax highlighter using PrismJS
+      </h3>
+
+      <div className="window">
+
+        <div className="title-bar">
+          <div className="title-buttons">
+            <div className="title-button"></div>
+            <div className="title-button"></div>
+            <div className="title-button"></div>
+          </div>
+        </div>
+
+        <div className="editor_wrap">
+          <Editor
+            value={code}
+            onValueChange={(code) => setCode(code)}
+            highlight={(code) => highlight(code, languages.js)}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
